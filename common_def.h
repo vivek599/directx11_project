c5a34acaafd989385a026659fe37437a4497f2ef
vivek599 <vivek6401@gmail.com>
2020-04-04 18:31:17 +0530

    fmath

--- common_def.h
+++ common_def.h
@@ -203,4 +203,35 @@ namespace
 		return retval;
 	
 	}
+
+	float sqrtbithack(float x)
+	{
+		//Dependant on IEEE representation and only works for 32 bits
+		unsigned int i = *(unsigned int*)&x;
+		// adjust bias
+		i += 127 << 23;
+		// approximation of square root
+		i >>= 1;
+		return *(float*)&i;
+	}
+
+	//Fast inverse square root implementation from Quake III Arena
+	float Q_rsqrt(float number)
+	{
+		long i;
+		float x2, y;
+		const float threehalfs = 1.5F;
+
+		x2 = number * 0.5F;
+		y = number;
+		i = *(long*)&y;                       // evil floating point bit level hacking
+		i = 0x5f3759df - (i >> 1);               // what the fuck? 
+		y = *(float*)&i;
+		y = y * (threehalfs - (x2 * y * y));   // 1st iteration
+	//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+		return y;
+	}
+
+
 }
